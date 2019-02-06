e4b0f4770f8c7f707c91d81c88febe2f527c8133
luiz <cieslakluiz@gmail.com>
2019-02-06 10:00:15 -0200

    update README.md

---
+++ class2/HelloWorld.js
@@ -0,0 +1,7 @@
+function HelloWorld() {
+  return <p>Hello World 🌍</p>;
+}
+
+function App() {
+  return <HelloWorld />;
+}
---
+++ class2/example-mdx-deck.js
@@ -0,0 +1,16 @@
+import { Head, Image } from 'mdx-deck'
+import { GFLogo, Center, Row, Container, Img } from './components'
+
+export { default as theme } from './theme'
+
+<Head>
+  <title>Class 1</title>
+</Head>
+
+<Container>
+  <Center>
+    <GFLogo src={'./img/gf-react2.png'} />
+  </Center>
+  <h2>Intensivão React</h2>
+  <p>Introdução</p>
+</Container>
