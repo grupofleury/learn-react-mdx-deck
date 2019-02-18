2705e6b0e81a4c272969c72706f8cdb034b4334d
luiz <cieslakluiz@gmail.com>
2019-02-18 16:17:10 -0300

    adaptando class2 para deploy e retirando live-code

--- packages/class2/class-component.js
+++ packages/class2/class-component.js
@@ -1,8 +1,15 @@
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: 'React'
+    };
+  }
+
   render() {
     return (
       <div>
-        <h1> oba </h1>
+        <h1> Hello {this.state.name}! </h1>
       </div>
     )
   }
