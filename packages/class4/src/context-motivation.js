3da38d838143b78a3b6bebe4b2810d3b4eb05b81
luiz <cieslakluiz@gmail.com>
2019-02-19 17:38:45 -0300

    add class4 mdx e js

---
+++ packages/class4/src/context-motivation.js
@@ -0,0 +1,18 @@
+const AppProd = () => {
+  return (
+    <App>
+      <Navbar />
+      <Container>
+        <Lista>
+          <Item>
+            <Avatar />
+            <Textos>
+              <Titulo />
+              <Subtitulo />
+            </Textos>
+          </Item>
+        </Lista>
+      </Container>
+    </App>
+  )
+}
